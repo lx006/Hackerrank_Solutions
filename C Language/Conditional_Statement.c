2babe5d274e3a5568d1ec37cb801b963f2a2c3d5
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-04-08 22:59:22 +0530

    Create Conditional_Statement.c

---
+++ C Language/Conditional_Statement.c
@@ -0,0 +1,67 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+int num=0;
+scanf("%d",&num);
+    if(num==1)
+    {
+        printf("one\n");
+        
+    }
+    else if(num==2)
+    {
+        printf("two\n");
+        
+    }
+    else if(num==3)
+    {
+        printf("three\n");
+        
+    }
+    else if(num==4)
+    {
+        printf("four\n");
+        
+    }
+    else if(num==5)
+    {
+        printf("five\n");
+        
+    }
+    else if(num==6)
+    {
+        printf("six\n");
+        
+    }
+    else if(num==7)
+    {
+        printf("seven\n");
+        
+    }
+    else if(num==8)
+    {
+        printf("eight\n");
+        
+    }
+    else if(num==9)
+    {
+        printf("nine\n");
+        
+    }
+    else 
+    {
+    printf("Greater than 9\n");
+    
+    }
+
+return 0;
+}
