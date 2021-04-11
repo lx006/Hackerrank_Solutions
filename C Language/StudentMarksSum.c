2af1877fbd444a842e3f974e15d290cb1703a07f
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-04-11 23:39:55 +0530

    Create StudentMarksSum.c

---
+++ C Language/StudentMarksSum.c
@@ -0,0 +1,30 @@
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) 
+{
+    int sum=0;
+    if(gender=='b')
+    {
+        for(int i=0;i<number_of_students;i+=2)
+        {
+            sum=sum+marks[i];
+        }
+    }   
+    else if(gender=='g')
+    {
+        for(int i=1;i<number_of_students;i+=2)
+        {
+            sum=sum+marks[i];
+        }
+    }
+    else 
+    {
+        sum=0;
+    }
+    
+    return sum;
+
+  
+}
+
