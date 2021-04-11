62afbb69a2afbfdffbd2b00059441a54385393e3
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-04-11 23:42:31 +0530

    Create VariableSizedArray.cpp

---
+++ C++/VariableSizedArray.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{
+     int n,q,k,i,j;
+    cin >> n >> q;
+    vector<vector<int> >arr(n);
+    for (i=0; i<n; i++)
+    {
+        cin >> k;
+        arr[i].resize(k);
+        for(j=0 ; j<k; j++)
+        {
+            cin >> arr[i][j];
+        }       
+    }
+
+    for(int v=0; v<q; v++)
+    {
+        cin >> i >> j;
+        cout << arr[i][j]<<endl;
+    }  
+    return 0;
+}
