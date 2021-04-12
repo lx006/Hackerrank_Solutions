74553ddde27ddcc707a116b26a156fc082409794
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-04-12 19:26:25 +0530

    Create Java_EndOfFile.java

---
+++ Java/Java_EndOfFile.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+
+    public static void main(String[] args) 
+    {
+        int i=1;
+        String ss;
+        Scanner scan=new Scanner(System.in);
+        while(scan.hasNext())
+        {
+            ss=scan.nextLine();
+            System.out.println(i+" "+ss);
+            i++;
+        }
+        
+
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
