ba35ccaf35aea391a63fa81b08afd6e7c19d0e80
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-08-03 21:01:23 +0530

    Update InsertNodeAtTail.cpp

--- Problem Solving/InsertNodeAtTail.cpp
+++ Problem Solving/InsertNodeAtTail.cpp
@@ -16,3 +16,39 @@ SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data)
         return head;
     }
 }
+
+
+//Alternative code using standard template library in c++
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<list>
+#include<iterator>
+using namespace std;
+
+
+int main() 
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    list<int> list1 ;
+    int n;
+    cin>>n;
+    while(n!=0)
+    {
+        int i;
+        cin>>i;
+        list1.push_back(i);
+        n--;
+    }
+    list<int> :: iterator it = list1.begin();
+    while(it!=list1.end())
+    {
+        cout<<*it<<endl;
+        it++;
+    }
+    return 0;
+}
+
