c47e1992c260cc97f7437358e7f46fd8274e8b40
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-08-03 20:25:15 +0530

    Create PrintTheElementOfLinkedList.cpp

---
+++ Problem Solving/PrintTheElementOfLinkedList.cpp
@@ -0,0 +1,13 @@
+
+    
+void printLinkedList(SinglyLinkedListNode* head) 
+{
+    SinglyLinkedListNode *temp = head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }    
+
+}
+
