b0737f3bdf87dc375da002a2e7b6d88c157a12f3
Laxmi Dawane <76866301+lx006@users.noreply.github.com>
2021-08-03 20:31:12 +0530

    Update PrintTheElementOfLinkedList.cpp

--- Problem Solving/PrintTheElementOfLinkedList.cpp
+++ Problem Solving/PrintTheElementOfLinkedList.cpp
@@ -11,3 +11,38 @@ void printLinkedList(SinglyLinkedListNode* head)
 
 }
 
+
+//alternative code using standard template library in c++
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<list>
+#include<iterator>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    list<int> list1; 
+    int n;
+    cin>>n;
+    while(n!=0)
+    {
+        int i;
+        cin>>i;
+        list1.push_back(i);
+        n--;
+    } 
+    list<int> :: iterator it=list1.begin();
+    while(it!=list1.end())
+    {
+        cout<<*it<<endl;
+        it++;
+    }
+    return 0;
+}
+
+
